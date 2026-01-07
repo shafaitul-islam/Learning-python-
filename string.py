a715fda8e56d643cc61497b82b050d45d01db521
shafaitul-islam <mshafaituli@gmail.com>
2026-01-07 20:38:11 +0600

    1st day

---
+++ string.py
@@ -0,0 +1,6 @@
+a="This is a string"
+b='This is also a string'
+print(a)
+print(type(a))
+print(b)
+print(type(b))
---
+++ string_index.py
@@ -0,0 +1,6 @@
+a='This is a string'
+print(a[0])
+print(a[5])
+print(len(a))
+print(a[len(a)-1])
+print(a[-1])
