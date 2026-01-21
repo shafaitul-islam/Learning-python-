aaa38cc7e41fc49d7e3583a33146c6110796bfb1
M Shafaitul Islam <mdshafaituli@gmail.com>
2026-01-21 18:51:20 +0600

    practice

---
+++ Learn Python/Python_Basics.py
@@ -0,0 +1,46 @@
+#First Program in Python
+print("Hello, World!")
+#Variables and Datas Types
+name= "Shafaitul"
+age = 23
+cgpa =3.00
+is_student = True
+print("Name:", name)
+print("Age:",age)
+print("CGPA:",cgpa)
+print("Is Student:",is_student)
+print(type(name),type(age),type(cgpa),type(is_student))
+#Type Convertions
+x ="10"
+y = int(x)
+print("Converted Value:",y+5)
+#Math Operations
+a=20
+b= 15
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor Division:",a//b)
+print ("Modulus:",a%b)
+print("Exponentiation:",a**b)
+#Input from User
+user_name = input ("Enter Your name:")
+user_age = input ("Enter Your age:")
+print("user Name:",user_name)
+print("user Age:",user_age)
+#convart string to integer
+f = "25"
+s = int(f)
+print("Sum:", s+10)
+print("Type Of S:", type(s))
+#check type of user input
+x= input("Enter a Value:")
+y = input ("Enter Another Value:")
+print("Type of X:", type(x))
+print("Type of Y:", type(y))
+#Change value of a list
+my_list =["shafayet",23,5.7,"CSE Student",True ]
+print("Orginal List:",my_list)
+my_list[2]= 5.9
+print("Updated List:",my_list)
