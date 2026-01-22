1944e192986585c7a3a67a235620b0cf85b21985
M Shafaitul Islam <mdshafaituli@gmail.com>
2026-01-22 19:25:32 +0600

    complete basic

--- Learn Python/Python_Basics.py
+++ Learn Python/Python_Basics.py
@@ -124,4 +124,125 @@ print("Student Name:", student["name"])
 for key, value in student.items():
     print(f"{key}:{value}")
 
-    
+# Functions 
+def add(a,b):
+    return a+b
+print("sum:", add(12,34))
+
+def greet (name):
+    print(f"Hello, {name}")
+    greet("Shafaitul")
+
+#Lambda Function
+square = lambda x: x*x
+print("Square of 5:", square(5))
+
+#Map Function
+numbers = [1,2,3,4,5]
+squared_numbers = list(map(lambda x: x**2, numbers))
+print("squared Numbers:", squared_numbers)
+
+#Filter Function
+even_numbers = list(filter(lambda x: x%2==0, numbers))
+print("Even Numbers:", even_numbers)
+
+#list comprehension
+cubes = [x**3 for x in range(1,6)]
+print("Cubes List:", cubes)
+
+#File Writing and Reading 
+#writing to a file
+file = open("data.txt","w")
+file.write("Hello, this is a sample file.\n")
+file.write("This file is created using python.\n")
+file.close()
+#Reading from a file
+file = open("data.txt","r")
+print(file.read())
+file.close()
+
+#Appending to a file
+file = open("data.text","a")
+file.write("Appending a new line to the file.\n")
+file.close()
+
+#Reading the update file
+file = open("data.txt","r")
+print(file.read())
+file.close()
+
+#Exception Handling
+try:
+    x = int(input("Enter a Number:"))
+    print("10 divided by", x, "is", 10/x)
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        except ValueError:
+            print("Error: Invalid input. please enter a valid number.")
+#Finally Block
+finally:
+    print("Execution Completed.")
+
+#Modules , json Date and Apis
+
+import math
+print("Square Root of 13:", math.sqrt(13))
+print("Value of PI:", math.pi)
+
+import json
+data = {
+    "name": "Shafaitul Islam",
+    "age": 23,
+    "department": "CSE",
+    "cgpa": 3.00
+}
+with open("student.json","w") as f:
+    json.dump(data,f)
+    from datetime import datetime
+    print("Current Date and Time:", datetime.now())
+
+    import requests 
+    response = requests.get("https://api.github.com")
+    print("GitHub API Status Code:", response.status_code)
+    print("GitHub API Response:", response.json())
+#OOp and Design Patterns
+
+#class Example
+class student:
+    def__init__(self,name,age):
+     self.name = name
+     self.age = age
+     def show__info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+        #Creating an object
+        s1 = student("Firus Anika Nesho",17)
+        s1.show_info()
+        #Inheritance Example
+        class person:
+            def__init__(self,name):
+            self.name = name
+            def greet(self):
+                print(f"Hello, my name is {self.name}")
+                class employee(person):
+                    def__init__(self,name,position):
+                        super().__init__(name)
+                        self.position = position
+                        def show_position(self):
+                            print(f"Position: {self.position}")
+                            e1 = employee("Shafaitul Islam", "software Engineer")
+                            e1.greet()
+                            e1.show_position()
+
+                            #Design Pattern Example: singleton
+                            class singletion:
+                                __instance = None
+                                def__new__(cls, *args, **kwargs):
+                                    if not cls.__instance:
+                                        cls.__instance = supper(singletion,cls).__new__(cls)
+                                        return cls.__instance
+                                        obj1 = singleton()
+                                        obj2 = singleton()
+                                        print("Are both objects same?", obj1 is obj2)
+                                        
+
+                    
